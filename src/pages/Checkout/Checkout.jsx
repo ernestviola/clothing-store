19d76c861c6f5719cdbf06b126eda504ad47df12
Ernest V <ernest.viol@gmail.com>
2026-04-17 09:23:21 -0700

    feat: Create a sliding windowed checkout form
    Created the form, but it affected the flex layout.

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import style from './checkout.module.css';
 import CartContext from '../../context/CartContext';
 import CheckoutItem from './CheckoutItem/CheckoutItem';
@@ -8,93 +8,122 @@ const Checkout = () => {
   const cart = cartOperations.cart;
   const totalCost = cartOperations.totalCost;
 
+  const [currentFormPage, setCurrentFormPage] = useState(0);
+
   return (
     <div className={style.container}>
       <div className={style.main}>
-        <h1> Checkout</h1>
+        <h1 className={style.header}> Checkout</h1>
         <div className={style.main__content}>
-          <form className={style.form}>
-            <fieldset>
-              <legend>Contact</legend>
-              <div className={style.row}>
-                <input type='text' aria-label='Email' placeholder='Email' />
+          <div className={style.form__container}>
+            <form className={style.formViewport}>
+              <div
+                className={style.formSlider}
+                style={{
+                  transform: `translateX(calc(${currentFormPage * -100}%/3))`,
+                }}
+              >
+                <fieldset>
+                  <legend>Contact</legend>
+                  <div className={style.row}>
+                    <input
+                      type='email'
+                      aria-label='Email'
+                      placeholder='Email'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input
+                      type='phone'
+                      aria-label='Phone Number'
+                      placeholder='Phone Number'
+                    />
+                  </div>
+                </fieldset>
+                <fieldset>
+                  <legend>Delivery</legend>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='First Name'
+                      placeholder='First Name'
+                    />
+                    <input
+                      type='text'
+                      aria-label='Last Name'
+                      placeholder='Last Name'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='Address Line 1'
+                      placeholder='Address Line 1'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='Address Line 2'
+                      placeholder='Address Line 2'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input type='text' aria-label='City' placeholder='City' />
+                    <input type='text' aria-label='State' placeholder='State' />
+                    <input
+                      type='text'
+                      aria-label='Zip Code'
+                      placeholder='Zip Code'
+                    />
+                  </div>
+                </fieldset>
+                <fieldset>
+                  <legend>Payment</legend>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='Credit Card Number'
+                      placeholder='Credit Card Number'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='Expiration Date'
+                      placeholder='Expiration Date'
+                    />
+                    <input
+                      type='text'
+                      aria-label='Security Code'
+                      placeholder='Security Code'
+                    />
+                  </div>
+                  <div className={style.row}>
+                    <input
+                      type='text'
+                      aria-label='Name on Card'
+                      placeholder='Name on Card'
+                    />
+                  </div>
+                </fieldset>
               </div>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Phone Number'
-                  placeholder='Phone Number'
-                />
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend>Delivery</legend>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='First Name'
-                  placeholder='First Name'
-                />
-                <input
-                  type='text'
-                  aria-label='Last Name'
-                  placeholder='Last Name'
-                />
-              </div>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Address Line 1'
-                  placeholder='Address Line 1'
-                />
-              </div>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Address Line 2'
-                  placeholder='Address Line 2'
-                />
-              </div>
-              <div className={style.row}>
-                <input type='text' aria-label='City' placeholder='City' />
-                <input type='text' aria-label='State' placeholder='State' />
-                <input
-                  type='text'
-                  aria-label='Zip Code'
-                  placeholder='Zip Code'
-                />
-              </div>
-            </fieldset>
-            <fieldset>
-              <legend>Payment</legend>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Credit Card Number'
-                  placeholder='Credit Card Number'
-                />
-              </div>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Expiration Date'
-                  placeholder='Expiration Date'
-                />
-                <input
-                  type='text'
-                  aria-label='Security Code'
-                  placeholder='Security Code'
-                />
-              </div>
-              <div className={style.row}>
-                <input
-                  type='text'
-                  aria-label='Name on Card'
-                  placeholder='Name on Card'
-                />
-              </div>
-            </fieldset>
-          </form>
+            </form>
+            <div className={style.indicator}>
+              <button
+                className={`${style.formCircle} ${currentFormPage === 0 ? style.formCircleActive : ''}`}
+                onClick={() => setCurrentFormPage(0)}
+              ></button>
+              <button
+                className={`${style.formCircle} ${currentFormPage === 1 ? style.formCircleActive : ''}`}
+                onClick={() => setCurrentFormPage(1)}
+              ></button>
+              <button
+                className={`${style.formCircle} ${currentFormPage === 2 ? style.formCircleActive : ''}`}
+                onClick={() => setCurrentFormPage(2)}
+              ></button>
+            </div>
+          </div>
           <div className={style.products}>
             <div>
               {Object.entries(cart).map(([, v]) => {
@@ -109,7 +138,7 @@ const Checkout = () => {
               })}
             </div>
             <span>Total ${totalCost}</span>
-            <button>Pay</button>
+            <button className={style.pay}>Pay</button>
           </div>
         </div>
       </div>
