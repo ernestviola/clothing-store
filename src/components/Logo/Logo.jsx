cba44318122811ae12ab9969284f3be0d55d9db8
Ernest V <ernest.viol@gmail.com>
2026-04-17 15:06:11 -0700

    feat: Add spin animation to the logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react';
 import style from './logo.module.css';
 
 const Logo = () => {
-  return <span className={style.logo}>STORE</span>;
+  const [rotation, setRotation] = useState(0);
+  const clickLogo = () => {
+    const rand = Math.floor(Math.random() * 1081);
+    setRotation(rand);
+  };
+
+  useEffect(() => {
+    let timeoutId;
+    if (rotation != 0) {
+      timeoutId = setTimeout(() => {
+        setRotation(0);
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [rotation]);
+
+  return (
+    <span
+      style={{ transform: `rotate(${rotation}deg)` }}
+      className={`${style.logo} ${style.load} `}
+      onClick={() => clickLogo()}
+    >
+      STORE
+    </span>
+  );
 };
 
 export default Logo;
