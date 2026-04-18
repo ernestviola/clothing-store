9d7db39ad6c003add10ba8b56012e16cb3f97db0
Ernest V <ernest.viol@gmail.com>
2026-04-17 17:42:12 -0700

    feat: Add opening and closing menu animations

--- src/components/Navbar/MenuDrawer/MenuDrawer.jsx
+++ src/components/Navbar/MenuDrawer/MenuDrawer.jsx
@@ -1,20 +1,37 @@
+import { useState } from 'react';
+
 import NavLinkDrawer from './NavLinkDrawer/NavLinkDrawer';
 import style from './menuDrawer.module.css';
 import { Close } from '@mui/icons-material';
 
 const MenuDrawer = ({ closeMenu }) => {
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleClose = async () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      closeMenu();
+    }, 200);
+  };
+
   return (
-    <div className={style.backDrop} onClick={closeMenu}>
-      <div className={style.menu} onClick={(e) => e.stopPropagation()}>
+    <div
+      className={`${style.backDrop} ${isClosing ? style.hideBackDrop : ''}`}
+      onClick={handleClose}
+    >
+      <div
+        className={`${style.menu} ${isClosing ? style.hide : ''}`}
+        onClick={(e) => e.stopPropagation()}
+      >
         <h2 className={style.header}>Menu</h2>
-        <button className={style.closeBtn} onClick={closeMenu}>
+        <button className={style.closeBtn} onClick={handleClose}>
           <Close />
         </button>
         <div className={style.links}>
-          <NavLinkDrawer to='/' timeout={0} closeMenu={closeMenu}>
+          <NavLinkDrawer to='/' timeout={0} closeMenu={handleClose}>
             Home
           </NavLinkDrawer>
-          <NavLinkDrawer to='/shop' timeout={300} closeMenu={closeMenu}>
+          <NavLinkDrawer to='/shop' timeout={300} closeMenu={handleClose}>
             Shop
           </NavLinkDrawer>
         </div>
